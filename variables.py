9f9bad833f750aae1d4d9a22fde4dd687bbef0c5
melinaasalihu <melinasalihu10@gmail.com>
2026-02-10 17:48:06 +0100

    python

---
+++ variables.py
@@ -0,0 +1,17 @@
+temperature = 44.2
+name = "Melina"
+
+print(type(temperature))
+print(type(name))
+
+age = 17
+print('age is:',age)
+
+age=age+1
+print('age now is',age)
+
+first_name = "Melina"
+last_name = "Salihu"
+full_name = first_name + ""+ last_name
+
+print(full_name)
