c8d3ffead328bccecd39f9b6593121427f4fc1e7
melinaasalihu <melinasalihu10@gmail.com>
2026-04-21 17:44:10 +0200

    commit

--- lesson14/sea_born.py
+++ lesson14/sea_born.py
@@ -1,13 +1,19 @@
-import sea_born as sns
+import seaborn as sns
 import pandas as pd
 import matplotlib.pyplot as plt
 
 df = pd.read_csv('avglQperCountry.csv')
 
+import seaborn as sns
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df = pd.read_csv('avglQpercountry.csv')
+
 print(df.info())
 
-#histogram of Average IQ
-plt.figure(figsize=(10,6))
+# Histogram of Average IQ
+plt.figure(figsize=(10, 6))
 sns.histplot(df['Average IQ'])
 plt.title('Histogram of Average IQ')
 plt.xlabel('Average IQ')
@@ -16,7 +22,7 @@ plt.tight_layout()
 plt.show()
 
 #heatmap
-df['Population - 2023'] = df['Population - 2023'].str.replace(',''').astype(float)
+df['Population - 2023'] = df['Population - 2023'].str.replace(',','').astype(float)
 print(df.info())
 
 numeric_iq_data_loc = df.select_dtypes(include=['number'])
@@ -32,6 +38,7 @@ sns.set_style('darkgrid')
 sns.boxplot(data=df, x='Continent', y='Average IQ')
 plt.title('Boxplot of Average IQ by Continent')
 plt.xlabel('Continent')
-plt.ylabel('AveraGe IQ')
+plt.ylabel('Average IQ')
 plt.tight_layout()
 plt.show()
+
